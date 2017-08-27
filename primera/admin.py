e142f96cf1e82134aa05a0913c10df2197c47be7
wariza <wjarizatruquetkd@gmail.com>
2017-08-27 17:42:59 -0500

    database creation
    database

--- primera/admin.py
+++ primera/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from primera.models import Modelo
+
+
+admin.site.register(Modelo)
 # Register your models here.
--- primera/models.py
+++ primera/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Modelo(models.Model):
+    entrada = models.CharField(max_length=10000)
+    salida = models.CharField(max_length=10000)
+    fechaCreacion = models.DateTimeField(auto_now_add=True)
+    usuario = models.CharField(max_length=45)
+
+    def __str__(self):
+        return self.nombre
