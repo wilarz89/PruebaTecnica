2fb651988b8a99a9f6d9953b3541c6ba30a9ae73
wariza <wjarizatruquetkd@gmail.com>
2017-08-27 13:16:20 -0500

    creacion apps
    Apps created

---
+++ primera/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class PrimeraConfig(AppConfig):
+    name = 'primera'
---
+++ segunda/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class SegundaConfig(AppConfig):
+    name = 'segunda'
