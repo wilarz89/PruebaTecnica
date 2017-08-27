0e47e02d1c0639e6a37caf8fcc009898ecbad396
wariza <wjarizatruquetkd@gmail.com>
2017-08-27 14:17:03 -0500

    views conf
    configuration

--- PruebaTecnica/urls.py
+++ PruebaTecnica/urls.py
@@ -1,3 +1,5 @@
+
+
 """PruebaTecnica URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
@@ -13,9 +15,11 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
+from primera import urls as primeraurls
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+url(r'^primera/', include(primeraurls)),
 ]
--- primera/views.py
+++ primera/views.py
@@ -5,4 +5,4 @@ from django.http import HttpResponse
 
 def index(request):
 
-    return render(request, 'primera/home.html', )
+    return render(request, 'primera/primera.html', )
